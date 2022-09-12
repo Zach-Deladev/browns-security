95e69343a76bce96582c574598b43d807fdb6f2d
Zach-Deladev <zach.delapenha.developer@gmail.com>
2022-09-12 01:05:22 +0000

    Added navbar to site

---
+++ browns/src/components/CallOut.js
@@ -0,0 +1,11 @@
+import styles from "./CallOut.module.css";
+
+function CallOut() {
+  return (
+    <div className={styles.CallOutDiv}>
+      <h1 className={styles.knockoutTextOne}>Browns</h1>
+    </div>
+  );
+}
+
+export default CallOut;
